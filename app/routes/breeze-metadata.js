3becb74330d8c1a067d0b824660e454df9502d86
pavlos.polianidis <pavlos.polianidis@brandview.com>
2014-12-30 14:31:44 +0000

    breeze-metadata enpoint added

---
+++ app/routes/breeze-metadata.js
@@ -0,0 +1,19 @@
+var db = require('../db'),
+	breezeMongoose = require('breeze-mongoose')(db.getModel);
+
+
+module.exports = function(app) {
+	app.get('breeze/metadata', function(req, res){
+		res.json(breezeMongoose.getMetadata(db.getModels()));
+	});
+
+	app.post('savechanges', function(req, res){
+        breezeMongoose.saveChanges(req.body)
+            .then(function(saveResults){
+             res.json(saveResults);
+        })
+        .catch(function(message){
+            res.send(500, message);
+        });
+    });
+};
