2e5f618c39eaf60572a19701272a0b1ed9add0d9
pavlos.polianidis <pavlos.polianidis@brandview.com>
2014-12-30 14:43:50 +0000

    breeze config added

---
+++ public/modules/data/breezeService.js
@@ -0,0 +1,153 @@
+(function(angular){
+	'use strict';
+
+	function breezeService($http, breeze, jsonResultsAdapterService){
+		 // region Consts
+
+        var METADATA_ENDPOINT = SERVICE_ENDPOINT + 'breeze/metadata'.
+        	SERVICE_ENDPOINT = 'http://localhost:3000';
+
+        // endregion
+
+        // region Inner Fields
+
+        var
+            _entityManager,
+            _metadata;
+
+        // endregion
+
+
+        // region Inner Methods
+
+        /**
+         * Starts the configuration process
+         * @private
+         */
+        function _configBreeze(){
+            _customizeAjaxAdapter();
+            _entityManager = _createEntityManager();
+            _registerCustomCtors();
+        }
+
+        /**
+         * Customizes the breezeJS ajax adapter
+         * @private
+         */
+        function _customizeAjaxAdapter(){
+            var ajax = breeze.config.initializeAdapterInstance('ajax', 'angular');
+
+            ajax.setHttp($http);
+        }
+
+        /**
+         * Creates a new breezeJS entity manager
+         * @private
+         */
+        function _createEntityManager(){
+            breeze.config.initializeAdapterInstance("modelLibrary", "backingStore", true);
+
+            // set custom naming convention
+            _getCustomNamingConvention().setAsDefault();
+
+            // define the breeze DataService
+            var
+                dataService = new breeze.DataService({
+                    serviceName: SERVICE_ENDPOINT,
+                    hasServerMetadata: false, // don't ask the server for metadata; we'll inject them manually
+                    jsonResultsAdapter: jsonResultsAdapterService
+                }),
+
+                // create the metadataStore
+                metadataStore = new breeze.MetadataStore();
+
+            // import metadata
+            metadataStore.importMetadata(_metadata);
+
+            // create a new EntityManager that uses this metadataStore
+            return new breeze.EntityManager({
+                dataService: dataService,
+                metadataStore: metadataStore
+            });
+        }
+
+        /**
+         * Registers custom constructors for our models
+         * @private
+         */
+        function _registerCustomCtors(){
+            var store = _entityManager.metadataStore;
+
+            for(var model in modelSet){
+                store.registerEntityTypeCtor(model, modelSet[model]);
+            }
+        }
+
+        /**
+         * Creates a custom naming convention
+         * @private
+         */
+         function _getCustomNamingConvention(){
+            var toCamelCase = function(propertyName){
+                return propertyName.charAt(0).toLowerCase() + propertyName.slice(1);
+            };
+
+            return new breeze.NamingConvention({
+                name: 'camelCaseOnBothSides',
+                serverPropertyNameToClient: toCamelCase,
+                clientPropertyNameToServer: toCamelCase
+            });
+        }
+
+        /**
+         * Fetches the metadata from the back-end
+         * @private
+         */
+        function _readMetadata(){
+            return $http.get(METADATA_ENDPOINT);
+        }
+
+		/**
+         * Performs initialization tasks
+         */
+        function init(){
+	        _readMetadata()
+	            .success(function(metadata){
+	                logger.info('Metadata Loaded');
+	                _metadata = metadata;
+	                _configBreeze();
+	            })
+	            .error(function(msg){
+	                logger.error('An error occured' + msg);
+	            });
+        }        
+
+         /**
+         * Creates a new entity of the given type
+         * @param type
+         * @param value
+         */
+        function createEntity(type, value){
+            if(!_metadata){
+                throw new Error('Metadata has not been loaded yet!');
+            }
+
+            return _entityManager.createEntity(type, value);
+        }
+
+
+        // endregion
+
+        return {
+        	init: init,
+            createEntity: createEntity
+        }
+	}
+
+	angular.module('data').service('breezeService', [
+            '$http',
+            'breeze',
+            'jsonResultsAdapterService'
+            breezeService
+		]);
+})(angular);
---
+++ public/modules/data/jsonResultsAdapter.js
@@ -0,0 +1,70 @@
+/**
+ * Custom jsonResultsAdapter for breezeJS
+ */
+(function (angular) {
+    'use strict';
+
+    function jsonResultsAdapterService() {
+
+        // region Inner Methods
+
+        function extractResults(data) {
+            var results = data.results.items;
+            if (!results) throw new Error("Unable to resolve 'results' property");
+
+            return results;
+        }
+
+        function visitNode(node, parseContext, nodeContext) {
+            var entityType = node.$type
+                ? _getEntityType(node.$type)
+                : _getEntityTypeAlt(parseContext.query.resourceName);
+
+            return {
+                entityType: entityType
+            };
+        }
+
+        /**
+         * Returns the entity type that is hidden in the
+         * given string; This is used when a saveObjectResult is returned
+         * @param type
+         * @private
+         */
+        function _getEntityType(type){
+            var tokens = type.split(','),
+                tokens2 = tokens[0].split('.');
+
+            return tokens2[tokens2.length - 1];
+        }
+
+        /**
+         * Returns the type of the node based on its resourceName;
+         * this is used when geting resources
+         * @param resourceName
+         * @private
+         */
+        function _getEntityTypeAlt(resourceName){
+            // ToDo: Find a better solution to map the resource name to entity name
+            switch(resourceName){
+                case 'articles':
+                    return 'Article';
+            }
+        }
+
+        // endregion
+
+        // region Public API
+
+        return new breeze.JsonResultsAdapter({
+            name: 'custom-adapter',
+            extractResults: extractResults,
+            visitNode: visitNode
+        });
+
+        // endregion
+    }
+
+    angular.module('data').service('jsonResultsAdapterService', [jsonResultsAdapterService]);
+
+})(angular);
